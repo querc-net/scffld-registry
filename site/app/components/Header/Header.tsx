36cd43a772779258e06e9876c80b63bc94842fb9
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-06-30 13:56:00 +1000

    feat: add logo

--- site/app/components/Header/Header.tsx
+++ site/app/components/Header/Header.tsx
@@ -5,12 +5,15 @@ import Link from 'next/link';
 
 import './Header.scss';
 import { ThemeToggle } from '../ThemeToggle/ThemeToggle';
+import { Logo } from '../../../src/components/src/components/Logo/Logo';
 
 export const Header: React.FC = () => {
   return (
     <header className="header">
       <h1>
-        <Link href="/">scffld | Registry</Link>
+        <Link href="/">
+          <Logo />
+        </Link>
       </h1>
       <Group>
         <ThemeToggle />
